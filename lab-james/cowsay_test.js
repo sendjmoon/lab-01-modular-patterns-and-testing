383dd396932b266ae108ef1ed5af3401cbdbf347
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-12 10:27:49 -0700

    added assert test

--- lab-james/cowsay_test.js
+++ lab-james/cowsay_test.js
@@ -1,8 +1,10 @@
 const expect = require('chai').expect;
+const assert = require('assert');
 const cowsay = require('./cowsay');
 
 describe('what the cow says', function() {
   it('should say what the user inputs', function() {
     expect(cowsay()).to.eql('moo cow says undefined');
+    assert.deepEqual(cowsay(), cowsay());
   });
 });
