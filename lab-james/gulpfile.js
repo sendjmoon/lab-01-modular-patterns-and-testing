304db86e7df7b0db08cb605e040376e069d39200
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-12 22:48:00 -0700

    added eslint.rc options to lint task in gulpfile.js

--- lab-james/gulpfile.js
+++ lab-james/gulpfile.js
@@ -8,7 +8,46 @@ var testFiles = ['./test/**/*.js'];
 
 gulp.task('lint', () => {
   return gulp.src(allFiles)
-   .pipe(eslint())
+   .pipe(eslint({
+     'rules': {
+       'no-console': 0,
+       'indent': [
+         2,
+         2
+       ],
+       'quotes': [
+         2,
+         'single'
+       ],
+       'linebreak-style': [
+         2,
+         'unix'
+       ],
+       'semi': [
+         2,
+         'always'
+       ]
+     },
+     'env': {
+       'es6': true,
+       'node': true,
+       'browser': true
+     },
+     'globals': {
+       'describe': false,
+       'it': false,
+       'beforeEach': false,
+       'afterEach': false,
+       'before': false,
+       'after': false
+     },
+     'ecmaFeatures': {
+       'modules': true,
+       'experimentalObjectRestSpread': true,
+       'impliedStrict': true
+     },
+     'extends': 'eslint:recommended'
+   }))
    .pipe(eslint.format());
 });
 
